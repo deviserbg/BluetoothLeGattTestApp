21d04ee710c8cc5db8221d8990f47ed00201758b
deviserbg <deviserbg@gmail.com>
2015-04-05 10:00:33 +0300

    Remove test vars

--- Application/src/main/java/com/example/android/bluetoothlegatt/DeviceScanActivity.java
+++ Application/src/main/java/com/example/android/bluetoothlegatt/DeviceScanActivity.java
@@ -63,9 +63,6 @@ public class DeviceScanActivity extends ListActivity {
     // Stops scanning after 10 seconds.
     private static final long SCAN_PERIOD = 10000;
 
-    private static final int test = 0;
-    private static final int test_new = 0;
-
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
