f56fad0b52d9b242f4b77df963dc33123fca0b36
deviserbg <deviserbg@gmail.com>
2015-04-18 07:46:11 +0300

    Test commit

--- Application/src/main/java/com/example/android/bluetoothlegatt/DeviceScanActivity.java
+++ Application/src/main/java/com/example/android/bluetoothlegatt/DeviceScanActivity.java
@@ -63,6 +63,7 @@ public class DeviceScanActivity extends ListActivity {
     // Stops scanning after 10 seconds.
     private static final long SCAN_PERIOD = 10000;
 
+    private static int trash = 0;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
